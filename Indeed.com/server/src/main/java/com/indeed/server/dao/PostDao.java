3e42c24a73d327fc0d0fce04a25bb67a01409f27
harshssn23 <144599747+harshssn23@users.noreply.github.com>
2024-10-25 15:43:50 +0530

    Add files via upload

---
+++ Indeed.com/server/src/main/java/com/indeed/server/dao/PostDao.java
@@ -0,0 +1,6 @@
+package com.indeed.server.dao;
+import com.indeed.server.model.PostModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PostDao extends MongoRepository<PostModel,String>{
+}
